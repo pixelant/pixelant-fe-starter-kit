cdfa3487c6b54d74ba7428227fb35eed3ebf5412
dmytro <dmytro@pixelant.se>
2015-07-15 18:19:12 +0300

    bug fixes

--- grunt/rename.js
+++ grunt/rename.js
@@ -1,26 +1,26 @@
 module.exports = {
-    allGitignore: {
-        src: 'app/templates/**/.gitignore',
-        dest: 'app/templates/**/gitignore'
+    // allGitignore: {
+    //     src: 'app/templates/**/.gitignore',
+    //     dest: 'app/templates/**/gitignore'
+    // },
+    // allGitignoreReverse: {
+    //     src: 'app/templates/**/gitignore',
+    //     dest: 'app/templates/**/.gitignore'
+    // },
+    felayoutGitignore: {
+        src: '<%= felayout %>/.gitignore',
+        dest: '<%= felayout %>/gitignore'
     },
-    allGitignoreReverse: {
-        src: 'app/templates/**/gitignore',
-        dest: 'app/templates/**/.gitignore'
+    felayoutGitignoreReverse: {
+        src: '<%= felayout %>/gitignore',
+        dest: '<%= felayout %>/.gitignore'
+    },
+    bigGitignore: {
+        src: '<%= big %>/.gitignore',
+        dest: '<%= big %>/gitignore'
+    },
+    bigGitignoreReverse: {
+        src: '<%= big %>/gitignore',
+        dest: '<%= big %>/.gitignore'
     }
-    // felayoutGitignore: {
-    //     src: '<%= felayout %>/.gitignore',
-    //     dest: '<%= felayout %>/gitignore'
-    // },
-    // felayoutGitignoreReverse: {
-    //     src: '<%= felayout %>/gitignore',
-    //     dest: '<%= felayout %>/.gitignore'
-    // }
-    // bigGitignore: {
-    //     src: '<%= felayout %>/.gitignore',
-    //     dest: '<%= felayout %>/gitignore'
-    // },
-    // bigGitignoreReverse: {
-    //     src: '<%= felayout %>/gitignore',
-    //     dest: '<%= felayout %>/.gitignore'
-    // }
 };
