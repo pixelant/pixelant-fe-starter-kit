6aaca3d45bb7a1a8c2314b81bb4bd9fa5447e3b4
dmytro <dmytro@pixelant.se>
2015-08-26 19:38:40 +0300

    update

--- app/templates/big/grunt/helpers/customizer/customizer.js
+++ app/templates/big/grunt/helpers/customizer/customizer.js
@@ -1,50 +1,37 @@
-    jQuery(function($) {
+/* global less */
 
-        // less.watch();
-        // var parent = $('.lll');
-        // var picker = new Picker(parent);
-        // $('').on('click', function(e){
-        // // parent.onclick = function() {
-        //     e.preventDefault()
-        //     picker.show();
-        // // };
-        // });
-        // var parent = $('#rrr');
-        var parent = document.getElementById('colorPicker');
-        var picker = new Picker(parent);
-        var bla;
-        parent.onclick = function(e) {
-          e.preventDefault()
-          picker.show();
-            bla = this.getAttribute('data-lessvar');
-        };
-        picker.on_done = function(colour) {
-            var lessObj = {};
-            lessObj[bla] = colour.rgba().toString();;
-            less.modifyVars(lessObj);
-            parent.style.background = colour.rgba().toString();
-            };
-        // picker.on_done = function(colour) {
-
-        //         // parent.style.background = colour.rgba().toString();
-        //         var lessObj = {};
-        //         lessObj['@main-color'] = colour.rgba().toString();;
-        //         less.modifyVars(lessObj);
-        // };
-        // $('.ttt').on('change', function(){
-        //     // var mainColor = '@main-color'
-        //     var lessObj = {};
-        //     lessObj[$(this).data('bbb')] = $(this).val();
-
-        //     // console.log(yourObject );
-        //     // console.log($(this).data('bbb'));
-        //     // var that = $(this);
-        //     // var rrr = $(this);
-        //     // lessKey = $(this).data('bbb');
-        //     // return console.log(lessKey);
-        //     less.modifyVars(lessObj);
-        // });
-
+var lessvar;
+var lessObj = {};
+jQuery(function($) {
+    $('a, button').on('click', function() {
+        return false;
     });
-    // jQuery(function($) {
-    // });
+    var $customizer = $('.js__customizer');
+    var $customizerHideBtn = $('.js__customizer__hide-btn');
+    var $colorPicker = $('.js__color-picker__val');
+    $customizerHideBtn.on('click', function(e) {
+        e.preventDefault();
+        $customizer .toggleClass('_hide-customizer');
+    });
+    $colorPicker.spectrum({
+        // flat: true,
+        showInput: true,
+        preferredFormat: 'hex',
+        showPalette: true,
+        palette: ['#ffffff'],
+        showSelectionPalette: true,
+        maxSelectionSize: 12,
+        showInitial: true,
+        localStorageKey: 'spectrum.customizer'
+    });
+    $colorPicker.on('move.spectrum', function(e, color) {
+        e.preventDefault();
+        $(this).css('background-color', color.toHexString());
+        lessvar = $(this).data('lessvar');
+        // var lessObj = {};
+        lessObj[lessvar] = color.toHexString();
+        less.modifyVars(lessObj);
+    });
+
+});
+
