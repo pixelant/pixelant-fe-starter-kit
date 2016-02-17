7d109ca09d8e17d16789252c29f591c5093f9b78
dmytro <dmytro@pixelant.se>
2016-02-17 16:50:20 +0200

    [CHORE] changelog fixes

--- grunt/changelog.js
+++ grunt/changelog.js
@@ -3,9 +3,18 @@ module.exports = {
         options: {
             version: null,
             changelog: 'CHANGELOG.md',
-            labels: ['[FEATURE]', '[FIX]', '[DOC]', '[STYLE]', '[CHORE]', '[TEST]', '[CLEANUP]'],
+            labels: ['[FEATURE]', '[FIX]', '[REFACTOR]', '[PERF]', '[DOC]', '[STYLE]', '[CHORE]', '[TEST]'],
             template: {
                 file: 'changelog.tpl',
+            },
+            matchers: {
+                label: function(body) {
+                    return body.match(/^([^\]]+)([^])/)[0];
+                },
+                subject: function(body) {
+                    var re = body.match(/^([^\]]+)([^])/g);
+                    return body.replace(new RegExp('\\' + re, 'g'), '');
+                },
             },
             write: true,
         }
@@ -14,9 +23,19 @@ module.exports = {
         options: {
             version: null,
             changelog: 'CHANGELOG.md',
-            labels: ['[FEATURE]', '[FIX]', '[DOC]', '[STYLE]', '[CHORE]', '[TEST]', '[CLEANUP]'],
+            labels: ['[FEATURE]', '[FIX]', '[REFACTOR]', '[PERF]', '[DOC]', '[STYLE]', '[CHORE]', '[TEST]'],
             template: {
                 file: 'changelog.tpl',
+            },
+            matchers: {
+                label: function(body) {
+                    return body.match(/^([^\]]+)([^])/)[0];
+                },
+                subject: function(body) {
+                    var re = body.match(/^([^\]]+)([^])/g);
+                    return body.replace(new RegExp('\\' + re, 'g'), '');
+
+                },
             },
             write: false,
         }
@@ -25,9 +44,11 @@ module.exports = {
 
 // Labels INFO
 
-// [FEATURE]: A new feature
-// [FIX]: A bug fix (bug fixes)
-// [DOC]: Documentation only changes
-// [STYLE]: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
-// [TEST]: Adding missing tests
-// [CHORE]: Changes to the build process or auxiliary tools and libraries such as documentation generation
+// [FEATURE] A new feature
+// [FIX] A bug fix
+// [REFACTOR] A code change that neither fixes a bug or adds a feature
+// [PERF] A code change that improves performance.
+// [CHORE] Changes to the build process (grunt) or auxiliary tools and libraries such as documentation generation and linters (jshint, etc.)
+// [DOC] Documentation only changes
+// [STYLE] Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
+// [TEST] Adding missing tests
