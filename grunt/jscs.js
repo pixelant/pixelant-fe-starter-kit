f4e4ec4560708ac39b1d43d5a75d10e8cb61f09f
dmh <dmytro.hrynevych@gmail.com>
2016-07-15 16:46:55 +0300

    [CHORE] disable force option in jscs and jshint

--- grunt/jscs.js
+++ grunt/jscs.js
@@ -1,7 +1,7 @@
 module.exports = {
     options: {
         config: '.jscsrc',
-        force: true
+        // force: true
     },
     js: {
         src: ['Gruntfile.js', 'grunt/*.js', 'app/index.js', 'test/*.js']
